6da2c29a8a2198cb460b8152aa6e2b5ba265c31f
Aina Rakotomalala <kantoniaina@hotmail.com>
2026-02-12 01:00:55 +0100

    Création page NotFound (erreur)

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
+import "./NotFound.scss";
+
 function NotFound() {
   return (
-    <div>
-      <h1>404</h1>
-      {/* Eventuellement un lien pour retourner à l'accueil */}
-    </div>
+    <main className="notfound">
+      <h1 className="notfound_title">404</h1>
+
+      <p className="notfound_text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+
+      <Link to="/" className="notfound_link">
+        Retourner sur la page d’accueil
+      </Link>
+    </main>
   );
 }
 
